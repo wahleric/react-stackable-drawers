16983b890941d660c62c5f4814645697c83fdee0
Eric Wahlquist <wahlquisteric@gmail.com>
2022-04-29 20:37:47 -0700

    Implement drawer callback

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import StackableDrawers, { drawerBus } from './component/StackableDrawers';
 
 function testAddDrawer() {
   drawerBus.openDrawer(<div>Hi! First drawer</div>, {
-    mount:'left'
+    mount:'left',
+    callback: (drawerData) => {
+      console.log('Hey, it worked');
+    }
   });
 }
 
